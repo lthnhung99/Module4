8eff632706486a19dc18e4054b0c254713e466c4
lthn99 <lenhung14101999@gmail.com>
2023-07-03 15:18:36 +0700

    bt banking

---
+++ banking/src/main/java/com/cg/service/customer/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.cg.service.customer;
+
+import com.cg.model.Customer;
+import com.cg.service.IGeneralService;
+
+import java.util.List;
+
+
+public interface ICustomerService extends IGeneralService<Customer, Long> {
+
+    List<Customer> findAllByFullNameLike(String fullName);
+
+    List<Customer> findAllByFullNameLikeOrEmailLikeOrPhoneLikeOrAddressLike(String fullName, String email, String phone,String address);
+}
